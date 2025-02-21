540656a0e0178fe077663f6866d51b2d71dacd44
Bright <adedamola@thankucash.com>
2025-02-21 01:37:31 +0100

    Onboarding

---
+++ app/src/main/java/com/mobile/recorduserapp/data/request/create_user/CreateUser.kt
@@ -0,0 +1,9 @@
+package com.mobile.recorduserapp.data.request.create_user
+
+data class CreateUser(
+    val country: String?,
+    val image: String?,
+    val latitude: Int?,
+    val longitude: Int?,
+    val name: String?
+)
--- app/src/main/java/com/mobile/recorduserapp/service/Network.kt
+++ app/src/main/java/com/mobile/recorduserapp/service/Network.kt
@@ -7,8 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Headers
-
-
+import retrofit2.http.POST
 
 
 interface Service{
@@ -20,6 +19,31 @@ interface Service{
     )
     @GET("/pupils?page=1")
     suspend  fun getUsers():Response<GetAllUsers>
+
+
+
+
+
+    @Headers(
+        "X-Request-ID: 1b3c82f4-b396-44eb-8e21-46100db3c899",
+        "User-Agent: Application-Name/1.0.0 (Operating System Name 1.0.0)"
+    )
+    @GET("/pupils")
+    suspend  fun createuser():Response<GetAllUsers>
+
+
+
+
+
+    @Headers(
+        "X-Request-ID: d8c4f59e-b07a-4b83-a452-9cb2f1db4cc1",
+        "User-Agent: Application-Name/1.0.0 (Operating System Name 1.0.0)"
+    )
+    @POST("/pupils?page=1")
+    suspend  fun creaateUser():Response<GetAllUsers>
+
+
+
 
 
 }
